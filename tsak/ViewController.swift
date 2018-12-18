2781f0885ae508b673701d35be293fabd72daf6a
ouyang <ouyang@ouyangdemac.local>
2018-12-17 20:16:14 -0800

    Initial Commit

---
+++ tsak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tsak
+//
+//  Created by ouyang on 2018/12/17.
+//  Copyright © 2018年 liuxinwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
